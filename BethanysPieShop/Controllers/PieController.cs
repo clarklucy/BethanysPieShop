03fc6e34a81d01a14ff2a469a7e5faba69a28cf7
L Clark <lucy.clark@york.ac.uk>
2020-05-07 17:21:36 +0100

    Added ViewModel. Updated Controller. Included ViewLayout, ViewImports, and ViewStart files

--- BethanysPieShop/Controllers/PieController.cs
+++ BethanysPieShop/Controllers/PieController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using BethanysPieShop.Models;
+using BethanysPieShop.ViewModels;
 using Microsoft.AspNetCore.Mvc;
 
 // For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
@@ -29,7 +30,11 @@ namespace BethanysPieShop.Controllers
 
         public ViewResult List()
         {
-            return View(_pieRepository.AllPies);
+            PiesListViewModel piesListViewModel = new PiesListViewModel();
+            piesListViewModel.Pies = _pieRepository.AllPies;
+            piesListViewModel.CurrentCategory = "Cheese Cakes";
+
+            return View(piesListViewModel); //this is now the ViewData.Model property of the view result being returned
         }
     }
 }
