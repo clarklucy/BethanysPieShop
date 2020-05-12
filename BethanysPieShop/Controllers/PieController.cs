68d5919341e8dc95d77528dd09c52465bb1b24e4
L Clark <lucy.clark@york.ac.uk>
2020-05-12 17:31:18 +0100

    Added View Components. Updated PieController.

--- BethanysPieShop/Controllers/PieController.cs
+++ BethanysPieShop/Controllers/PieController.cs
@@ -28,14 +28,15 @@ namespace BethanysPieShop.Controllers
         }
 
 
-        public ViewResult List()
-        {
-            PiesListViewModel piesListViewModel = new PiesListViewModel();
-            piesListViewModel.Pies = _pieRepository.AllPies;
-            piesListViewModel.CurrentCategory = "Cheese Cakes";
+        //---- original List action
+        //public ViewResult List()
+        //{
+        //    PiesListViewModel piesListViewModel = new PiesListViewModel();
+        //    piesListViewModel.Pies = _pieRepository.AllPies;
+        //    piesListViewModel.CurrentCategory = "Cheese Cakes";
 
-            return View(piesListViewModel); //this is now the ViewData.Model property of the view result being returned
-        }
+        //    return View(piesListViewModel); //this is now the ViewData.Model property of the view result being returned
+        //}
 
         public IActionResult Details (int id)
         {
@@ -43,6 +44,31 @@ namespace BethanysPieShop.Controllers
             if (pie == null)
                 return NotFound();
             return View(pie);
+        }
+
+
+        public ViewResult List(string category)
+        {
+            IEnumerable<Pie> pies;
+            string currentCategory;
+
+            if (string.IsNullOrEmpty(category))
+            {
+                pies = _pieRepository.AllPies.OrderBy(p => p.PieId);
+                currentCategory = "All pies";
+            }
+            else
+            {
+                pies = _pieRepository.AllPies.Where(p => p.Category.CategoryName == category)
+                    .OrderBy(p => p.PieId);
+                currentCategory = _categoryRepository.AllCategories.FirstOrDefault(c => c.CategoryName == category)?.CategoryName;
+            }
+
+            return View(new PiesListViewModel
+            { 
+                Pies = pies,
+                CurrentCategory = currentCategory
+            });
         }
     }
 }
